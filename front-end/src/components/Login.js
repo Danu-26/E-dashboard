618409a8a903d3b06812e12336a8de9311b91a97
Danu Lingam <danulingam@Danus-MacBook-Air.local>
2024-02-06 13:57:07 +0530

    first commit

---
+++ front-end/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login=()=>{
+    return (
+        <div className='login'>
+            <h1>Login</h1>
+            <input className="inputBox"  type="text" placeholder="Enter your email"/>
+            <input className="inputBox"  type="password" placeholder="Enter password "  />
+        </div>
+    )
+    
+}
+
+export default Login;
